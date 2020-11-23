c4258da770a90d4b627a2e3490937cb82e75d4cc
Yuta Nakano <nakano.yuta252@gmail.com>
2020-11-24 01:57:10 +0900

    Create Login and Signup

---
+++ src/features/auth/authSlice.ts
@@ -0,0 +1,182 @@
+import { createSlice, PayloadAction, createAsyncThunk, bindActionCreators } from '@reduxjs/toolkit';
+import { RootState } from '../../app/store';
+import axios from "axios";
+import {
+    AUTH_STATE,
+    CRED,
+    LOGIN_USER,
+    POST_PROFILE,
+    PROFILE,
+    JWT,
+    USER
+} from "../types";
+import { profile } from 'console';
+
+
+export const fetchAsyncLogin = createAsyncThunk(
+    "auth/login",
+    async (auth: CRED) => {
+        const res = await axios.post<JWT>(
+            `${process.env.REACT_APP_API_URL}/authen/jwt/create`,
+            auth,
+            {
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            }
+        );
+        return res.data
+    }
+);
+
+export const fetchAsyncRegister = createAsyncThunk(
+    "auth/register",
+    async (auth: CRED) => {
+        const res = await axios.post<USER>(
+            `${process.env.REACT_APP_API_URL}/api/create`,
+            auth,
+            {
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            }
+        );
+        return res.data
+    }
+);
+
+export const fetchAsyncGetMyProf = createAsyncThunk(
+    "auth/loginuser",
+    async () => {
+        const res = await axios.post<LOGIN_USER>(
+            `${process.env.REACT_APP_API_URL}/api/loginuser/`,
+            {
+                headers: {
+                    Authorization: `JWT ${localStorage.localJWT}`,
+                },
+            }
+        );
+        return res.data
+    }
+);
+
+
+export const fetchAsyncCrateMyProf = createAsyncThunk(
+    "auth/createProfile",
+    async () => {
+        const res = await axios.post<PROFILE>(
+            `${process.env.REACT_APP_API_URL}/api/profile/`,
+            { img: null},
+            {
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `JWT ${localStorage.localJWT}`,
+                },
+            }
+        );
+        return res.data
+    }
+);
+
+/* プロフィール一覧取得 */
+export const fetchAsyncGetProfs = createAsyncThunk(
+    "auth/getProfiles",
+    async () => {
+        const res = await axios.get<PROFILE[]>(
+            `${process.env.REACT_APP_API_URL}/api/profile/`,
+            {
+                headers: {
+                    Authorization: `JWT ${localStorage.localJWT}`,
+                },
+            }
+        );
+        return res.data
+    }
+);
+
+/* プロフィールのアップデート */
+export const fetchAsyncUpdateProf = createAsyncThunk(
+    "auth/updateProfile",
+    async (profile: POST_PROFILE) => {
+        const uploadData = new FormData();
+        profile.img && uploadData.append("img", profile.img, profile.img.name)
+        const res = await axios.put<PROFILE>(
+            `${process.env.REACT_APP_API_URL}/api/profile/${profile.id}`,
+            uploadData,
+            {
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `JWT ${localStorage.localJWT}`,
+                },
+            }
+        );
+        return res.data
+    }
+);
+
+
+const initialState: AUTH_STATE = {
+    isLoginView: true,
+    loginUser: {
+        id: 0,
+        username: "",
+    },
+    profiles: [ {id: 0, user_profile: 0, img: null} ],
+};
+
+export const authSlice = createSlice({
+    name: 'auth',
+    initialState,
+    reducers: {
+        toggleMode(state) {
+            state.isLoginView = !state.isLoginView;
+        },
+    },
+    extraReducers: (builder) => {
+        builder.addCase(
+            fetchAsyncLogin.fulfilled,
+            (state, action: PayloadAction<JWT>) => {
+                localStorage.setItem("localJWT", action.payload.access);
+                action.payload.access && (window.location.href = "/tasks");
+            }
+        );
+        builder.addCase(
+            fetchAsyncGetMyProf.fulfilled,
+            (state, action: PayloadAction<LOGIN_USER>) => {
+                return {
+                    ...state,
+                    loginUser: action.payload,
+                }
+            }
+        );
+        builder.addCase(
+            fetchAsyncGetProfs.fulfilled,
+            (state, action: PayloadAction<PROFILE[]>) => {
+                return {
+                    ...state,
+                    profiles: action.payload,
+                }
+            }
+        );
+        builder.addCase(
+            fetchAsyncUpdateProf.fulfilled,
+            (state, action: PayloadAction<PROFILE>) => {
+                return {
+                    ...state,
+                    profiles: state.profiles.map( (prof) =>
+                        prof.id === action.payload.id ? action.payload : prof
+                    ),
+                };
+            }
+        );
+    },
+});
+
+export const { toggleMode } = authSlice.actions;
+
+
+export const selectIsLoginView = (state: RootState) => state.auth.isLoginView;
+export const selectIsLoginUser = (state: RootState) => state.auth.loginUser;
+export const selectProfiles = (state: RootState) => state.auth.profiles;
+
+export default authSlice.reducer;
